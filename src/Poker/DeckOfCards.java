519554a6759da9e96dd50a118401950cff021296
Harry Gleeson <harry.gleeson@hotmail.com>
2017-02-07 16:34:45 +0000

    Additional commmenting

--- src/Poker/DeckOfCards.java
+++ src/Poker/DeckOfCards.java
@@ -1,3 +1,4 @@
+// Harry Gleeson - 14455822 - COMP30050 - Assignment 2
 package Poker;
 
 import java.util.Random;
@@ -11,8 +12,8 @@ public class DeckOfCards {
 	private char[] suits = new char[] {PlayingCard.HEARTS, PlayingCard.DIAMONDS, PlayingCard.CLUBS, PlayingCard.SPADES};
 	private int x=0, i=0, z = 0;
 	
-	private void initializeDeck(){
-		while(z<52){ //Iterates through array of PlayingCards and populates it with the 52 cards in a deck 
+	private void initializeDeck(){ //Private method that uses loops to populate an array with all of the cards in a deck
+		while(z<52){ 
 			for(i=0; i<13; i++){
 				cards[z] = new PlayingCard(type[i], suits[x], faceValues[i], gameValues[i]);
 				arrayIndex[z] = z;
@@ -23,7 +24,7 @@ public class DeckOfCards {
 			}
 	}
 	
-	public DeckOfCards(){ //Constructor uses loop to create all 52 cards in a deck
+	public DeckOfCards(){ //Constructor uses private method to create all 52 cards in a deck
 		initializeDeck();
 	}
 	
@@ -69,13 +70,13 @@ public class DeckOfCards {
 		CardDeck.reset(); //Resets the deck to ensure all cards are present then shuffles them
 		int counter=0;
 		PlayingCard deal;
-		while(counter < 52){
+		while(counter < 52){ //Deals all of the cards in the deck and prints their toString values before returning the card to be discarded
 		deal = CardDeck.dealNext();
 		System.out.println(deal);
 		CardDeck.returnCard(deal);
 		counter++;
 		}
-		System.out.println(CardDeck.dealNext());
+		System.out.println(CardDeck.dealNext()); //Tests what happens when all cards have been dealt and another one is dealt
 		
 
 	}
